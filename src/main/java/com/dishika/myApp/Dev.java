a4dbcc7542852227e1d3e29ba5825f98788e2f1d
dishika1 <dishika1@innovasolutions.com>
2026-07-01 16:43:55 +0530

    @Component in spring boot

---
+++ src/main/java/com/dishika/myApp/Dev.java
@@ -0,0 +1,10 @@
+package com.dishika.myApp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+    public void build(){
+        System.out.println("Building a project");
+    }
+}
--- src/main/java/com/dishika/myApp/MyAppApplication.java
+++ src/main/java/com/dishika/myApp/MyAppApplication.java
@@ -2,12 +2,22 @@ package com.dishika.myApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class MyAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MyAppApplication.class, args);
+		ApplicationContext context =SpringApplication.run(MyAppApplication.class, args);
+		
+		//normally creating an object
+		// Dev dev = new Dev();
+        // dev.build();
+
+		//using beans
+		Dev obj = context.getBean(Dev.class);
+		obj.build();
+
 	}
 
 }
